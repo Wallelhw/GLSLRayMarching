1bb4cc9f97b3760f67396bb11a3028813d76aa02
guanronlin <guanronlin@tencent.com>
2021-03-12 14:34:35 +0800

    remove

--- 3rdparty/zlib/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/Play/GLSLRayMarching/3rdparty/zlib
-# Build directory: G:/Play/GLSLRayMarching/3rdparty/zlib/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(example "example")
-set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "G:/Play/GLSLRayMarching/3rdparty/zlib/CMakeLists.txt;235;add_test;G:/Play/GLSLRayMarching/3rdparty/zlib/CMakeLists.txt;0;")
